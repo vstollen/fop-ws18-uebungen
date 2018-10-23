9185842354e37e2588255141bdc3ee9fe9e11f06
Vincent <vstollen@gmail.com>
2018-10-23 10:07:06 +0200

    Add H00 Vorlagedatei

---
+++ Uebungsblatt_0/H00_Stollenwerk_Vincent.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname U00_NACHNAME_VORNAME) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Type:
+;; Returns: 
+(define (generate-filename nr ln fn)
+  ;; Insert your Code here
+  )
