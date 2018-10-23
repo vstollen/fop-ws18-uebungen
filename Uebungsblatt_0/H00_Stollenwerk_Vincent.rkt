4a730bf10a5790f0c2c4081e8af52e088cb947ec
Vincent <vstollen@gmail.com>
2018-10-23 10:32:03 +0200

    Finish Uebung 0 H1

--- Uebungsblatt_0/H00_Stollenwerk_Vincent.rkt
+++ Uebungsblatt_0/H00_Stollenwerk_Vincent.rkt
@@ -1,8 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname U00_NACHNAME_VORNAME) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
-;; Type:
-;; Returns: 
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname H00_Stollenwerk_Vincent) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Type: natural string string -> string
+;; Returns: Correct filename for homework submission
 (define (generate-filename nr ln fn)
-  ;; Insert your Code here
-  )
+  (string-append "H" (if (> nr 9) "" "0") (number->string nr) "_" ln "_" fn ".rkt"))
+
+(check-expect (generate-filename 0 "Stollenwerk" "Vincent") "H00_Stollenwerk_Vincent.rkt")
+(check-expect (generate-filename 9 "Müller" "Hans") "H09_Müller_Hans.rkt")
+(check-expect (generate-filename 13 "Mayer" "Gertrud") "H13_Mayer_Gertrud.rkt")
