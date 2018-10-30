cc2068f32e756c1537fa9b1cab131cedeff2116b
Vincent <vstollen@gmail.com>
2018-10-30 20:50:10 +0100

    Add contracts to Uebung 1 V3

--- Uebungsblatt_01/V3.rkt
+++ Uebungsblatt_01/V3.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname V3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Type: number number number number -> number
+;; Returns: The distance between two two dimensional points
 (define (circles-distance x1 y1 x2 y2)
   (sqrt (+ (* (- x2 x1) (- x2 x1)) (* (- y2 y1) (- y2 y1)))))
 
@@ -8,6 +10,8 @@
 (check-expect (circles-distance 0 0 3 4) 5)
 (check-expect (circles-distance 5 6 25 27) 29)
 
+;; Type: number number number number number number -> String
+;; Returns: Description of the relative position of two circles
 (define (circles-position x1 y1 r1 x2 y2 r2)
   (if (< (circles-distance x1 y1 x2 y2) (abs (- r1 r2)))
       "Interior"
