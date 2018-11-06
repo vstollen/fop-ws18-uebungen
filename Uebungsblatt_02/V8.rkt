ce50ab5b040b648b95bb1407186fd94eee1efd34
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-06 13:55:04 +0100

    Complete Uebung 2 V8

--- Uebungsblatt_02/V8.rkt
+++ Uebungsblatt_02/V8.rkt
@@ -4,15 +4,19 @@
 (define-struct br (id pop type))
 
 ;; Type: (list of br) -> number
+;; Precondition: Book types "Subscription" or "Single"
 ;; Returns: Total lending fee given list of br
 (define (fee-total book-lending-list)
   ;; Type: br number -> number
+  ;; Precondition: Book types "Subscription" or "Single"
   ;; Returns: Book fee added to a given number
-  (foldr (lambda (br total-fee)
-           (+ total-fee (if (= br-type "Subscription")
+  (foldr (lambda (book total-fee)
+           (+ total-fee (if (string=? (br-type book) "Subscription")
                             1.5
-                            (* br-pop 1.75))))
+                            (* (br-pop book) 1.75))))
          0
          book-lending-list))
 
 (check-expect (fee-total (list (make-br 4 2 "Subscription") (make-br 7 3 "Single"))) 6.75)
+(check-expect (fee-total empty) 0)
+(check-expect (fee-total (list (make-br 38 5 "Subscription") (make-br 42 3 "Subscription") (make-br 4 1 "Single"))) 4.75)
