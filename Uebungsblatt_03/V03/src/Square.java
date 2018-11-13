cdc9238901f09db00c0a66385d6eb79fd2b58e42
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-13 21:01:58 +0100

    Add Uebung 3 V5

--- Uebungsblatt_03/V03/src/Square.java
+++ Uebungsblatt_03/V03/src/Square.java
@@ -10,7 +10,23 @@ public class Square implements Directions {
 		World.setVisible(true);
 
 		// TODO: V5 insert your code here
-		
+
+		Robot[] robots = new Robot[2];
+		robots[0] = new Robot(1, 1, Directions.East, 20);
+		robots[1] = new Robot(10, 10, Directions.West, 20);
+
+		for (int i = 0; i < 2; i+=1) {
+			for (int j = 0; j < 9; j+=1) {
+				for (Robot robot : robots) {
+					robot.move();
+					robot.putBeeper();
+				}
+			}
+
+			for (Robot robot : robots) {
+				robot.turnLeft();
+			}
+		}
 	}
 
 }
