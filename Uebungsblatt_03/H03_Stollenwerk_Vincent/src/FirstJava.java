1ba7e49459d29657e42a7772c92d1d09d9af9c51
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-13 21:58:56 +0100

    Import H3 template

---
+++ Uebungsblatt_03/H03_Stollenwerk_Vincent/src/FirstJava.java
@@ -0,0 +1,16 @@
+import kareltherobot.Directions;
+import kareltherobot.Robot;
+import kareltherobot.World;
+
+public class FirstJava implements Directions {
+
+	public static void main(String[] args) {
+		World.setDelay(5);
+		World.setSize(7, 5);
+		World.setVisible(true);
+
+		// TODO: H1 insert your code here
+
+	}
+
+}
