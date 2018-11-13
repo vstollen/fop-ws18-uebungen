8f0c0f82c3b74ef0b16988307231a280e9a6edca
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-13 22:12:40 +0100

    Add Uebung 3 H1

--- Uebungsblatt_03/H03_Stollenwerk_Vincent/src/FirstJava.java
+++ Uebungsblatt_03/H03_Stollenwerk_Vincent/src/FirstJava.java
@@ -11,6 +11,21 @@ public class FirstJava implements Directions {
 
 		// TODO: H1 insert your code here
 
+		Robot[] robots = new Robot[5];
+
+		// Da Arrays bei 0 anfangen entspricht das i in der Aufgabenstellung i + 1 im Code
+		for (int i = 0; i < 5; i+=1) {
+			robots[i] = new Robot(1, i+1, Directions.North, (i+1)*(i+2));
+		}
+
+		for (int i = 0; i < robots.length; i+=1) {
+			while (robots[i].anyBeepersInBeeperBag()) {
+				for (int j = 0; j < i+1; j+=1) {
+					robots[i].putBeeper();
+				}
+				robots[i].move();
+			}
+		}
 	}
 
 }
