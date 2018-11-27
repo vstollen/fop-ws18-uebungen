c3a309a5b3474ea3485c8ff03e5bdeca1c41f98b
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-27 21:56:55 +0100

    Add Uebung 5 V3

---
+++ Uebungsblatt_05/V05/src/EclipseReady.java
@@ -0,0 +1,9 @@
+
+public class EclipseReady {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+
+	}
+
+}
---
+++ Uebungsblatt_05/V05/src/V3.java
@@ -0,0 +1,10 @@
+public class V3 {
+
+    public static void main(String[] args) {
+        int[] a = new int[42];
+        
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 2*i+1;
+        }
+    }
+}
