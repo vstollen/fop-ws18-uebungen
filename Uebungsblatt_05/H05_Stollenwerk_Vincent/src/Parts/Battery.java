deac74595befac3fc8a2721fdc9847c990cafa46
Vincent Stollenwerk <vstollen@gmail.com>
2018-11-28 14:21:33 +0100

    Add Uebung 5 H1

---
+++ Uebungsblatt_05/H05_Stollenwerk_Vincent/src/Parts/Battery.java
@@ -0,0 +1,28 @@
+package Parts;
+
+public class Battery extends Part {
+
+    private int level;
+
+    public Battery(String condition, int level) {
+        super("Battery", condition);
+
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+
+        if (level < 100) {
+            if (level > 0) {
+                setCondition(Part.conditionUsed);
+            } else {
+                setCondition(Part.conditionDamaged);
+            }
+        }
+    }
+}
