cc47827e90ae0e70969d8d2ac102049393ead727
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-04 14:56:21 +0100

    Add Uebung 6 H1.2

--- Uebungsblatt_06/H06_Stollenwerk_Vincent/src/H1/ApproximatePi.java
+++ Uebungsblatt_06/H06_Stollenwerk_Vincent/src/H1/ApproximatePi.java
@@ -30,7 +30,7 @@ public class ApproximatePi {
 
 		double pi = (pointsInsideCircle / n) * 4;
 
-		System.out.println(pi);
+		System.out.println("Monte Carlo - " + pi);
 
 		return pi;
 	}
@@ -46,8 +46,38 @@ public class ApproximatePi {
 	}
 
 	// TODO H1.2
+
+	/**
+	 * Approximates pi by approximating an equivalent integral using the trapezoidal rule
+	 * @param n number of trapezes to use
+	 * @return approximation of pi
+	 */
 	double integrationPi(int n) {
-		return 3;
+
+		// Prevents division by zero
+		if (n < 1) {
+			n = 1;
+		}
+
+		double sum = 0;
+		for (double k = 1; k <= n; k++) {
+			sum += piFunction((k - 1) / n);
+			sum += piFunction(k / n);
+		}
+
+		double pi = sum / (2*n);
+
+		System.out.println("Integration - " + pi);
+		return pi;
+	}
+
+	/**
+	 * Calculates the y value of f(x) = 4 / (1 + x^2)
+	 * @param x value to be placed into the function
+	 * @return functions y value
+	 */
+	double piFunction(double x) {
+		return 4 / (1 + x*x);
 	}
 
 
