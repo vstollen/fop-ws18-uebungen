39a918efe173a36a6e45bd8b5e5c8fb179d5ae96
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-12 15:31:52 +0100

    Add basic testing

---
+++ Uebungsblatt_07/H07_Stollenwerk_Vincent/src/Tests/TermTests.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import Exceptions.InvalidTermException;
+import Main.Term;
+
+public class TermTests {
+
+	@Test
+	public void simpleNumberTest() {
+		assertTimeout(Duration.ofSeconds(10), () ->  new Term("-120"));
+	}
+	
+	@Test
+	public void wrongBraceTest() {
+		InvalidTermException exception = assertThrows(InvalidTermException.class, () -> new Term("3*(2 + 7))"));
+		
+		assertEquals(exception.getMessage(), "Wrong number of closing braces");
+	}
+	
+	@Test
+	public void multipleOperatorTest() {
+		InvalidTermException exception = assertThrows(InvalidTermException.class, () -> new Term("3**2"));
+		
+		assertEquals(exception.getMessage(), "Incorrect operator usage");
+	}
+	
+	@Test
+	public void leadingOperatorTest() {
+		InvalidTermException exception = assertThrows(InvalidTermException.class, () -> new Term("+42"));
+		
+		assertEquals(exception.getMessage(), "Incorrect operator usage");
+	}
+}
