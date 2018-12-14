db13ab03a0b88a32d78a72921a4d8b3f5c41ddfa
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-14 13:27:07 +0100

    Implement Uebung 7 H5.2

--- Uebungsblatt_07/H07_Stollenwerk_Vincent/src/Main/TermIO.java
+++ Uebungsblatt_07/H07_Stollenwerk_Vincent/src/Main/TermIO.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import Exceptions.InvalidTermException;
@@ -34,6 +35,27 @@ public class TermIO {
 			return false;
 		} catch (UnsupportedEncodingException e) {
 			System.out.println("Unsupported encoding: " + e.getMessage());
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param filePath
+	 * @return results of terms written into filePath
+	 */
+	public boolean writeTermResultsToFile(String filePath) {
+		
+		String[] output = new String[terms.length];
+		
+		for (int i = 0; i < terms.length; i++) {
+			output[i] = terms[i].getResult();
+		}
+		try {
+			Utils.writeLinesToFile(filePath, output);
+		} catch (IOException e) {
+			System.out.println("Error writing to file: " + e.getMessage());
 			return false;
 		}
 		
