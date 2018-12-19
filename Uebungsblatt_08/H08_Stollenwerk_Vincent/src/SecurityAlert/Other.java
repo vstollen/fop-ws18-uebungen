47aef78aa74d485e07b0bf5cb1efa009324876aa
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-19 15:45:08 +0100

    Implement Uebung 8 H3.2

--- Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/Other.java
+++ Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/Other.java
@@ -2,22 +2,53 @@ package SecurityAlert;
 
 public class Other extends GovernmentEmployee {
 
+	/**
+	 * @param name
+	 * @param sex
+	 */
+	public Other(String name, Sex sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+	
+	private static final SecurityLevel SECURITY_LEVEL = SecurityLevel.LOW;
+	
 	@Override
 	public int compareTo(GovernmentEmployee o) {
-		// TODO Auto-generated method stub
+		
+		if (o.getSecurityLevel().ordinal() > SECURITY_LEVEL.ordinal()) {
+			return 1;
+		}
+		
+		if (o.getSecurityLevel().ordinal() < SECURITY_LEVEL.ordinal()) {
+			return -1;
+		}
+		
 		return 0;
 	}
 
 	@Override
 	SecurityLevel getSecurityLevel() {
-		// TODO Auto-generated method stub
-		return null;
+		return SECURITY_LEVEL;
 	}
 
 	@Override
 	String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		switch (sex) {
+			case FEMALE:
+				sb.append("Madame ");
+				break;
+			case MALE:
+				sb.append("Mister ");
+				break;
+		}
+		
+		sb.append(name);
+		
+		return sb.toString();
 	}
 	
 
--- Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/Secretary.java
+++ Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/Secretary.java
@@ -2,22 +2,54 @@ package SecurityAlert;
 
 public class Secretary extends GovernmentEmployee {
 
+	/**
+	 * @param name
+	 * @param sex
+	 */
+	public Secretary(String name, Sex sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+	
+	private static final SecurityLevel SECURITY_LEVEL = SecurityLevel.MEDIUM;
+	private static final String TITLE = "Secretary";
+	
 	@Override
 	public int compareTo(GovernmentEmployee o) {
-		// TODO Auto-generated method stub
+		
+		if (o.getSecurityLevel().ordinal() > SECURITY_LEVEL.ordinal()) {
+			return 1;
+		}
+		
+		if (o.getSecurityLevel().ordinal() < SECURITY_LEVEL.ordinal()) {
+			return -1;
+		}
+		
 		return 0;
 	}
 
 	@Override
 	SecurityLevel getSecurityLevel() {
-		// TODO Auto-generated method stub
-		return null;
+		return SECURITY_LEVEL;
 	}
 
 	@Override
 	String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		switch (sex) {
+			case FEMALE:
+				sb.append("Madame ");
+				break;
+			case MALE:
+				sb.append("Mister ");
+				break;
+		}
+		
+		sb.append(TITLE).append(" ").append(name);
+		
+		return sb.toString();
 	}
 	
 
