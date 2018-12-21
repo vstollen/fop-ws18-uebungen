b4f66d9de99727d88333b4841d07cdb3de931cf4
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-21 12:11:19 +0100

    Implement Uebung 8 H3.4

--- Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/EmergencyQueue.java
+++ Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/EmergencyQueue.java
@@ -4,4 +4,30 @@ public class EmergencyQueue<T extends GovernmentEmployee> {
 
 	private PriorityQueue<GovernmentEmployee> queue;
 	
+	public void rescue(int n) {
+		for (int i = 0; i < n && queue.getSize() > 0; i++) {
+			
+			GovernmentEmployee rescuedEmployee = queue.dequeue();
+			
+			StringBuilder rescueAlert = new StringBuilder();
+			
+			rescueAlert.append(rescuedEmployee.getTitle());
+			rescueAlert.append(" was rescued");
+			
+			System.out.println(rescueAlert.toString());
+		}
+	}
+	
+	public void chooseDesignatedSurvivor(Secretary ds) {
+		queue.addToHeadOfPriorityClass(ds);
+	}
+	
+	public void enqueue(GovernmentEmployee newItem) {
+		queue.enqueue(newItem);
+	}
+	
+	@Override
+	public String toString() {
+		return queue.toString();
+	}
 }
