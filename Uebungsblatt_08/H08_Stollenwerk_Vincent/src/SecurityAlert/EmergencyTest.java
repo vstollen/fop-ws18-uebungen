08b07057802109a81a1c47d910b7f3170f4693f0
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-21 11:31:17 +0100

    Add Uebung 8 PriorityQueue enqueue Unit tests (H3.6)

--- Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/EmergencyTest.java
+++ Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/EmergencyTest.java
@@ -1,5 +1,10 @@
 package SecurityAlert;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +32,51 @@ public class EmergencyTest {
 	Other ryan = new Other("Paul Ryan", Sex.MALE); // Speaker of the House of Representatives
 	Other sessions = new Other("Hillary Clinton", Sex.FEMALE);
 	Other hatch = new Other("Orin Hatch", Sex.MALE); // President pro tempore of the United States Senate
+	
+	@Test
+	@SuppressWarnings("unchecked")
+	public void priorityQueueEnqueueTest() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		PriorityQueue<GovernmentEmployee> testQueue = new PriorityQueue<>();
+		
+		testQueue.enqueue(ryan);
+		testQueue.enqueue(trump);
+		testQueue.enqueue(nielsen);
+		testQueue.enqueue(hatch);
+		testQueue.enqueue(chao);
+		
+		Field internalListField = PriorityQueue.class.getDeclaredField("queue");
+		internalListField.setAccessible(true);
+		LinkedList<GovernmentEmployee> internalList = (LinkedList<GovernmentEmployee>) internalListField.get(testQueue);
+		
+		LinkedList<GovernmentEmployee> correctList = new LinkedList<>();
+		correctList.add(trump);
+		correctList.add(nielsen);
+		correctList.add(chao);
+		correctList.add(ryan);
+		correctList.add(hatch);
+		
+		assertEquals(correctList, internalList);
+		
+		
+		testQueue = new PriorityQueue<>();
+		
+		testQueue.enqueue(hatch);
+		testQueue.enqueue(nielsen);
+		testQueue.enqueue(ryan);
+		testQueue.enqueue(hatch);
+		testQueue.enqueue(nielsen);
+		testQueue.enqueue(trump);
+		
+		internalList = (LinkedList<GovernmentEmployee>) internalListField.get(testQueue);
+		
+		correctList = new LinkedList<>();
+		correctList.add(trump);
+		correctList.add(nielsen);
+		correctList.add(hatch);
+		correctList.add(ryan);
+		
+		assertEquals(correctList, internalList);
+	}
 	
 
 }
