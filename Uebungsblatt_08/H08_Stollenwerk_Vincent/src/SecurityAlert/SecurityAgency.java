d79d97a741fe68f9af0d10bec0e1357b263e4c1d
Vincent Stollenwerk <vstollen@gmail.com>
2018-12-21 13:17:44 +0100

    Implement Uebung 8 H3.5

--- Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/SecurityAgency.java
+++ Uebungsblatt_08/H08_Stollenwerk_Vincent/src/SecurityAlert/SecurityAgency.java
@@ -6,8 +6,48 @@ public class SecurityAgency {
 
 	private ArrayList<GovernmentEmployee> emps;
 	private EmergencyQueue<GovernmentEmployee> emergency;
-	private AlertLevel al;
+	private AlertLevel al = AlertLevel.LOW;
 
-
+	/**
+	 * Adds e to internal employee list
+	 * @param e Employee
+	 */
+	void add(GovernmentEmployee e) {
+		emps.add(e);
+	}
+	
+	/**
+	 * Submit a change to the AlertLevel
+	 * @param newAL new Alert Level
+	 */
+	public void changeAlertLevel(AlertLevel newAL) {
+		al = newAL;
+		
+		if (al.ordinal() <= AlertLevel.ELEVATED.ordinal()) {
+			
+			for (GovernmentEmployee employee : emps) {
+				if (employee instanceof President) {
+					emergency.enqueue(employee);
+				}
+			}
+			
+		}
+		
+		if (al.ordinal() <= AlertLevel.HIGH.ordinal()) {
+			
+			for (GovernmentEmployee employee : emps) {
+				if (employee instanceof Secretary) {
+					emergency.enqueue(employee);
+				}
+			}
+		}
+		
+		if (al.ordinal() <= AlertLevel.SEVERE.ordinal()) {
+			
+			for (GovernmentEmployee employee : emps) {
+				emergency.enqueue(employee);
+			}
+		}
+	}
 
 }
