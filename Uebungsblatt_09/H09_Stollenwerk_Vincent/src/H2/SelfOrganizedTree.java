dcfdecfa84a0ef74e67ec26e2d42b7a5b7929599
Vincent Stollenwerk <vstollen@gmail.com>
2019-01-18 15:56:32 +0100

    Clean up Uebung 9 H2

--- Uebungsblatt_09/H09_Stollenwerk_Vincent/src/H2/SelfOrganizedTree.java
+++ Uebungsblatt_09/H09_Stollenwerk_Vincent/src/H2/SelfOrganizedTree.java
@@ -4,31 +4,105 @@ public class SelfOrganizedTree<T extends Comparable<T>> extends BinaryTree<T> {
 
 	@Override
 	public boolean search(T value) {
-		/*
-		 * TODO H2.1, H2.2, H2.3
-		 */
 
-		if (root.value.equals(value)) {
+		BinaryTreeNode<T> pointer = root;
+
+		if (value.compareTo(root.value) == 0) {
 			return true;
 		}
 
-		if (root.left.value.equals(value)) {
-			zickRotation();
-			return true;
+		if (value.compareTo(root.value) < 0) {
+			pointer = root.left;
 		}
 
-		if (root.right.value.equals(value)) {
-			zackRotation();
-			return true;
+		if (value.compareTo(root.value) > 0) {
+			pointer = root.right;
 		}
 
+		BinaryTreeNode<T> grandGrandParent = null;
+		BinaryTreeNode<T> grandparent = null;
+		BinaryTreeNode<T> parent = root;
+
+		while (pointer != null) {
+			if (value.compareTo(pointer.value) == 0) {
+
+				if (grandparent == null) {
+					if (value.compareTo(root.value) < 0) {
+						root = zickRotation(root);
+					} else if (value.compareTo(root.value) > 0) {
+						root = zackRotation(root);
+					}
+				} else {
+					if (value.compareTo(grandparent.left.value) < 0) {
+
+						grandparent = zickRotation(grandparent);
+						grandparent = zickRotation(grandparent);
+
+						insertNode(value, grandGrandParent, grandparent);
+					} else if (value.compareTo(grandparent.right.value) > 0) {
+
+						grandparent = zackRotation(grandparent);
+						grandparent = zackRotation(grandparent);
+
+						insertNode(value, grandGrandParent, grandparent);
+					} else if (value.compareTo(parent.value) > 0) {
+
+						parent = zackRotation(parent);
+						grandparent.left = parent;
+
+						grandparent = zickRotation(grandparent);
+
+						insertNode(value, grandGrandParent, grandparent);
+					} else if (value.compareTo(parent.value) < 0) {
+
+						parent = zickRotation(parent);
+						grandparent.right = parent;
+
+						grandparent = zackRotation(grandparent);
+						insertNode(value, grandGrandParent, grandparent);
+					}
+				}
+				return true;
+			}
+
+			grandGrandParent = grandparent;
+			grandparent = parent;
+			parent = pointer;
+
+			if (value.compareTo(pointer.value) < 0) {
+				pointer = pointer.left;
+			} else {
+
+				if (value.compareTo(pointer.value) > 0) {
+					pointer = pointer.right;
+				}
+			}
+		}
 		return false;
 	}
 
 	/**
-	 *  Performs a zick rotation (right rotation)
+	 * Inserts node based of it's value
+	 * @param value
+	 * @param parent
+	 * @param insertNode
 	 */
-	private void zickRotation() {
+	private void insertNode(T value, BinaryTreeNode<T> parent, BinaryTreeNode<T> insertNode) {
+		if (parent == null) {
+			root = insertNode;
+		} else if (value.compareTo(parent.value) < 0) {
+			parent.left = insertNode;
+		} else {
+			parent.right = insertNode;
+		}
+	}
+
+	/**
+	 * Performs a zick rotation (right rotation)
+	 * @param root root node
+	 * @return root node after rotation
+	 */
+	private BinaryTreeNode<T> zickRotation(BinaryTreeNode<T> root) {
 
 		BinaryTreeNode<T> oldRoot = root;
 		BinaryTreeNode<T> leftRootChild = root.left;
@@ -37,12 +111,16 @@ public class SelfOrganizedTree<T extends Comparable<T>> extends BinaryTree<T> {
 		leftRootChild.right = oldRoot;
 
 		root = leftRootChild;
+
+		return root;
 	}
 
 	/**
 	 * Performs a zack rotation (left rotation)
+	 * @param root root node
+	 * @return root node after rotation
 	 */
-	private void zackRotation() {
+	private BinaryTreeNode<T> zackRotation(BinaryTreeNode<T> root) {
 
 		BinaryTreeNode<T> oldRoot = root;
 		BinaryTreeNode<T> rightRootChild = root.right;
@@ -51,6 +129,8 @@ public class SelfOrganizedTree<T extends Comparable<T>> extends BinaryTree<T> {
 		rightRootChild.left = oldRoot;
 
 		root = rightRootChild;
+
+		return root;
 	}
 
 }
