268154337d8a5f52564b42b2514af2d9a84faf6b
Vincent Stollenwerk <vstollen@gmail.com>
2019-02-13 19:10:00 +0100

    Implement Uebung 13 H2.1 Dragoncurve getSequence

--- Uebungsblatt_13/H13_Stollenwerk_Vincent/src/H2/DragonCurve.java
+++ Uebungsblatt_13/H13_Stollenwerk_Vincent/src/H2/DragonCurve.java
@@ -4,7 +4,22 @@ public class DragonCurve {
 
 	// TODO H2.1
 	public String getSequence(int n) {
-		return "";
+
+		if (n < 1) {
+			return "";
+		}
+
+		if (n == 1) {
+			return "R";
+		}
+
+		String leadingSequence = getSequence(n -1);
+
+		StringBuilder newSequence = new StringBuilder(leadingSequence);
+
+		newSequence.setCharAt(leadingSequence.length() / 2, 'L');
+
+		return leadingSequence + "R" + newSequence.toString();
 	}
 
 }
