6e9ed0896fc1672665f64ee8e94ab0b77a1c8995
Vincent Stollenwerk <vstollen@gmail.com>
2019-02-13 21:15:33 +0100

    Implement Uebung 13 H3 Sierpinski Carpet

--- Uebungsblatt_13/H13_Stollenwerk_Vincent/src/H3/SierpinskiCarpet.java
+++ Uebungsblatt_13/H13_Stollenwerk_Vincent/src/H3/SierpinskiCarpet.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
 public class SierpinskiCarpet extends JFrame {
 
 	private int len = 729;
-	private int n = 2;
+	private int n = 3;
 
 	public SierpinskiCarpet() {
 		super("Sierpinski Carpet");
@@ -21,10 +21,38 @@ public class SierpinskiCarpet extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
-	// TODO H3
 	@Override
 	public void paint(Graphics g) {
-		
+		// ContantPane.getSize() returns the actual size of the canvas
+		// while getWidth() and getHeight() include the title and the borders.
+		Dimension dimens = getContentPane().getSize();
+
+		// Starts to paint at the correct offset from the title bar
+		paintAt(g, (int) (0.5 * (getWidth() - dimens.getWidth())), (int) (getHeight() - dimens.getHeight()), len, n);
+	}
+
+	private void paintAt(Graphics g ,int x, int y, int size, int n) {
+
+		if (n == 0) {
+			return;
+		}
+
+		int tileSize = size / 3;
+
+		g.fillRect(x + tileSize, y + tileSize, tileSize, tileSize);
+
+		if (n > 1) {
+			paintAt(g, x, y, tileSize, n-1);
+			paintAt(g, x + tileSize, y, tileSize, n-1);
+			paintAt(g, x + 2 * tileSize, y, tileSize, n-1);
+
+			paintAt(g, x, y + tileSize, tileSize, n-1);
+			paintAt(g, x + 2 * tileSize, y + tileSize, tileSize, n-1);
+
+			paintAt(g, x, y + 2 * tileSize, tileSize, n-1);
+			paintAt(g, x + tileSize, y + 2 * tileSize, tileSize, n-1);
+			paintAt(g, x + 2 * tileSize, y + 2 * tileSize, tileSize, n-1);
+		}
 	}
 
 	
